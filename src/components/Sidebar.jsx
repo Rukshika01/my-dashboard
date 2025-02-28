f4eea2ec97b4ea18fa5cb6f6bb03dac32d7e9318
Rukshika <rukshika.kumar@gmail.com>
2025-02-28 10:55:27 +0530

    added more elements to the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { Box, VStack, Link, Icon } from "@chakra-ui/react";
 import { MdOutlineDashboard } from "react-icons/md";
 import { FaDatabase, FaMap } from "react-icons/fa6";
 import { RiSurveyLine } from "react-icons/ri";
-
+import { IoLibraryOutline } from "react-icons/io5";
 
 function Sidebar() {
   return (
@@ -67,7 +67,38 @@ function Sidebar() {
         >
           <Icon as={RiSurveyLine} /> Surveys
         </Link>
-
+        <Link 
+          href="#" 
+          display="flex" 
+          alignItems="center" 
+          gap={3} 
+          p={3} 
+          w="full" 
+          borderRadius="md"
+          _hover={{ 
+            bg: "#8B4513", 
+            transform: "scale(1.05)", 
+            boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" 
+          }}
+        >
+          <Icon as={IoLibraryOutline} /> Library
+        </Link>
+        <Link 
+          href="#" 
+          display="flex" 
+          alignItems="center" 
+          gap={3} 
+          p={3} 
+          w="full" 
+          borderRadius="md"
+          _hover={{ 
+            bg: "#8B4513", 
+            transform: "scale(1.05)", 
+            boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" 
+          }}
+        >
+          <Icon as={FaMap} /> Marketplace
+        </Link>
         
       </VStack>
     </Box>
